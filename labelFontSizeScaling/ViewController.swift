acf6bba56ec4fac6b559e96e77cd8a273a6e3b74
Yulia Milovanova <ymilovanova@actionlearning.ru>
2021-01-20 12:31:44 +0300

    Initial Commit

---
+++ labelFontSizeScaling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  labelFontSizeScaling
+//
+//  Created by Preacher on 20.01.2021.
+//  Copyright © 2021 Yulia Milovanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
